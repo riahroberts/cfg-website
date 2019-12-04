9455602e6794ffdc5e29dfc7acf3cd7055bd3d61
riahroberts <56638725+riahroberts@users.noreply.github.com>
2019-12-04 19:28:16 +0000

    added javascript

---
+++ Javascript/jquery.js
@@ -0,0 +1,16 @@
+
+
+$('document').ready(function(){
+console.log("HI");
+
+// meat or no meat
+$('#meatbutton').click(function(){
+  $('#veggiebutton').prop("disabled", true);
+});
+
+$('#veggiebutton').click(function(){
+  $('#meatbutton').prop("disabled", true);
+});
+
+
+})
